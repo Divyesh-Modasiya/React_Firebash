a09ccdff08d3754dfe45a1f3ab0b2de0bcf5e2b7
Divyesh Modasiya <divyeshmodasiya@gmail.com>
2024-09-18 13:17:55 +0530

    google auth

---
+++ Google Auth/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Login from './Login'
+import 'slick-carousel/slick/slick.css'; 
+import 'slick-carousel/slick/slick-theme.css';
+
+export default function App() {
+  return (
+    <div>
+      <Login/>
+    </div>  
+  )
+}
